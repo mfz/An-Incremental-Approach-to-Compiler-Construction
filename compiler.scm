7b91e15685eb10eb3f6f96ce6f27cf0724044a5d
Florian Zink <zink.florian@gmail.com>
2021-03-16 22:22:43 +0000

    1.2

--- compiler.scm
+++ compiler.scm
@@ -1,10 +1,41 @@
 
 
+(define fxshift 2)
+(define fxmask #x03)
+(define bool_f #x2F)
+(define bool_t #x6F)
+(define wordsize 4)
+(define niltag  #b00111111)
+(define chartag #b00001111)
+(define charshift 8)
+
+
+(define fixnum-bits (- (* wordsize 8) fxshift))
+(define fxlower (- (expt 2 (- fixnum-bits 1))))
+(define fxupper (sub1 (expt 2 (- fixnum-bits 1))))
+
+(define (fixnum? x)
+  (and (integer? x) (exact? x) (<= fxlower x fxupper)))
+
+(define (immediate? x)
+  (or (fixnum? x) (boolean? x) (char? x) (null? x)))
+
+(define (immediate-rep x)
+  (cond
+   [(fixnum? x) (ash x fxshift)]
+   [(boolean? x) (if x bool_t bool_f)]
+   [(null? x) niltag]
+   [(char? x) (logor (ash (char->integer x) charshift) chartag)]))
+
+
+
+
 (define (compile-program x)
-  (unless (integer? x) (error ))
+  (unless (immediate? x) (error "Not an immediate"))
   (emit "define i32 @scheme_entry(){")
-  (emit "   ret i32 ~s" x)
+  (emit "   ret i32 ~s" (immediate-rep x))
   (emit "}"))
 
 (load "tests-driver.scm")
 (load "tests-1.1-req.scm")
+(load "tests-1.2-req.scm")
--- startup.c
+++ startup.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
+#include <ctype.h>
+
+/* define scheme constants */
+
+#define bool_f     0x2F
+#define bool_t     0x6F
+
+#define fx_mask    0x03
+#define fx_tag     0x00
+#define fx_shift      2
+
+#define nil_tag     0b00111111
+
+#define char_mask   0xFF
+#define char_tag    0b00001111
+#define char_shift  8
+
+const char *ascii_table[0x7F] = {
+ "nul",       "soh",    "stx",     "etx",   "eot", "enq",    "ack",  "bel",
+ "backspace", "tab",    "newline", "vt",    "np",  "return", "so",   "si",
+ "dle",       "dc1",    "dc2",     "dc3",   "dc4", "nak",    "syn",  "etb",
+ "can",       "em",     "sub",     "esc",   "fs",  "gs",     "rs",   "us",
+ "space"
+};
+
+
+
+
+
+typedef unsigned int ptr;
+
+static void print_ptr(ptr x){
+  if ((x & fx_mask) == fx_tag){
+    printf("%d", ((int)x) >> fx_shift);
+  } else if(x == bool_f){
+    printf("#f");
+  } else if(x == bool_t){
+    printf("#t");
+  } else if(x == nil_tag){
+    printf("()");
+  } else if((x & char_mask) == char_tag){
+    char c = (char)(x >> char_shift);
+    if (iscntrl(c) || isspace(c)){
+      printf("#\\%s", ascii_table[c]);
+    } else {
+      printf("#\\%c", c);
+    }
+  } else {
+    printf("#<unknown 0x%08x>", x);
+  }
+  printf("\n");
+}
+
+
+  
 
 int main(int argc, char** argv){
-  printf("%d\n", scheme_entry());
+  print_ptr(scheme_entry());
   return 0;
 }
